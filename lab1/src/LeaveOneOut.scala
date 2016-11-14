abb212315981913906cbf32203d56ad3a40001b2
NoNick <aleksandrs634@gmail.com>
2016-11-14 09:42:56 +0300

    rm is much faster than refactoring on each trait change

--- lab1/src/LeaveOneOut.scala
+++
@@ -1,9 +0,0 @@
-import Main._
-
-object LeaveOneOut extends SingleParameterFitter {
-    override def costsByParameters(params: Seq[Int], classifier: kNN, data: MarkedDataSet,
-                                   cost: (MarkedDataSet, MarkedDataSet) => Double): Seq[Double] =
-        costsByParametersImpl(params, classifier, data, cost, data.length)
-
-    override def getName: String = "LOO"
-}
